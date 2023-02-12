872b614cc6d277e7d58e47bcb4d3bc54b807f997
Muhammad Ewaily <muhammad.ewaily@ripple-innovations.com>
2023-02-12 23:52:09 +0200

    [add] app icon and splash screen

---
+++ MoviX/MoviX/Delegates/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MoviX
+//
+//  Created by Muhammad Ewaily on 12/02/2023.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
